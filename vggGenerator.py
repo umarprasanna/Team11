687739221fe63097e011d88086d1d5732826b81a
Adam Dawood <ayd5335@CSE-dynpool09.cse.psu.edu>
2018-11-19 12:44:37 -0500

    adds vgg training def and generator

---
+++ vggGenerator.py
@@ -0,0 +1,63 @@
+import numpy as np
+import keras
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.transform import resize
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras import backend as K
+import pdb
+
+class vggGenerator(keras.utils.Sequence):
+
+    def __init__(self, list_IDs, data_dir, batch_size=32, dim=(48,48), patch_size= 48, n_channels=1, n_classes=2, shuffle=True):
+        self.dim = dim
+        self.patch_size = patch_size
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.data_dir = data_dir
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+    def __len__(self):
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        X, y = self.__data_generation__(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation__(self, list_IDs_temp):
+        X = np.empty(self.dim + (3, self.batch_size))
+        y = np.empty((self.batch_size,))
+
+        for i, ID in enumerate(list_IDs_temp):
+            # Load X
+            img = load_img(self.data_dir + '/images/' + ID, grayscale=True)
+            x_img = img_to_array(img)
+            x_img = np.repeat(resize(x_img, (128, 128, 1), mode='constant', preserve_range=True), 3, 2)
+            start_1 = int(np.random.rand() * (x_img.shape[0] - self.patch_size))
+            start_2 = int(np.random.rand() * (x_img.shape[1] - self.patch_size))
+            end_1 = start_1 + self.patch_size
+            end_2 = start_2 + self.patch_size
+            X[:, :, :, i] = x_img[start_1:end_1, start_2:end_2, :]
+            mask = img_to_array(load_img(self.data_dir + '/masks/' + ID, grayscale=True))
+            mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+            y[i] = mask[start_1 + int(self.patch_size/2), start_2 + int(self.patch_size/2), 0]
+        return X.reshape((self.batch_size, *self.dim, 3)), y
+
---
+++ vgg_train_def.py
@@ -0,0 +1,125 @@
+import os
+#matrix math
+import numpy as np
+#read/write image data
+import imageio
+#visualize data
+import matplotlib.pyplot as plt
+#data preprocessing 
+import pandas as pd
+#deep learning
+import torch
+import TGSSaltDataset as tg
+from keras.applications import vgg16 as vgg16
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+#just in case we need a backup datasets
+import tensorflow as tf
+from torch.utils import data
+import pdb
+#visualization
+def plot2x2Array(image, mask):
+    #invoke matplotlib!
+    f, axarr = plt.subplots(1,2)
+    axarr[0].imshow(image)
+    axarr[1].imshow(mask)
+    axarr[0].grid()
+    axarr[1].grid()
+    axarr[0].set_title('Image')
+    axarr[1].set_title('Mask')
+
+#decoding
+def rleToMask(rleString,height,width):
+    #width heigh
+    rows,cols = height,width
+    try:
+        #get numbers
+        rleNumbers = [int(numstring) for numstring in rleString.split(' ')]
+        #get pairs
+        rlePairs = np.array(rleNumbers).reshape(-1,2)
+        #create an image
+        img = np.zeros(rows*cols,dtype=np.uint8)
+        #for each pair
+        for index,length in rlePairs:
+            #get the pixel value 
+            index -= 1
+            img[index:index+length] = 255
+        
+        
+        #reshape
+        img = img.reshape(cols,rows)
+        img = img.T
+    
+    #else return empty image
+    except:
+        img = np.zeros((cols,rows))
+    
+    return img
+
+
+
+train_mask = pd.read_csv('train.csv')
+#depth data
+depth = pd.read_csv('depths.csv')
+#training path
+train_path = "./train/"
+
+#list of files
+file_list = list(train_mask['id'].values)
+#define our dataset using our class
+dataset = tg.TGSSaltDataset(train_path, file_list)
+
+import sys
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.transform import resize
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras import backend as K
+from vggGenerator import vggGenerator
+
+train_ids = next(os.walk(train_path+"images"))[2]
+test_ids = train_ids[int(len(train_ids) * .85):len(train_ids)]
+train_ids = train_ids[0:int(len(train_ids) * .85)] 
+im_width = 128
+im_height = 128
+border = 5
+im_chan = 3 # Number of channels: first is original and second cumsum(axis=0)
+n_features = 1 # Number of extra features, like depth
+#path_train = '../input/train/'
+#path_test = '../input/test/'
+trainG = vggGenerator(train_ids, train_path)
+testG = vggGenerator(test_ids, train_path)
+
+callbacks = [
+    EarlyStopping(patience=5, verbose=1),
+    ReduceLROnPlateau(patience=3, verbose=1),
+    ModelCheckpoint('model-tgs-salt-1.h5', verbose=1, save_best_only=True, save_weights_only=True)
+]
+
+model = vgg16.VGG16(False, input_shape=(48, 48, 3))
+model = Sequential(model.layers)
+for layer in model.layers:
+         layer.trainable= False
+model.add(Flatten())
+model.add(Dense(4048, activation='relu'))
+#model.add(Activation('softmax'))
+model.add(Dense(1024, activation='relu'))
+#model.add(Activation('softmax'))
+model.add(Dense(1024, activation='relu'))
+#model.add(Activation('relu'))
+model.add(Dense(512, activation='relu'))
+#model.add(Activation('softmax'))
+model.add(Dense(1))
+model.add(Activation('relu'))
+model.summary()
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) # The mean_iou metrics seens to leak train and test values...
+#results = model.fit({'input_1': X_train, 'feat': X_feat_train}, y_train, batch_size=16, epochs=50, callbacks=callbacks, validation_data=({'img': X_valid, 'feat': X_feat_valid}, y_valid))
+#NEED TO REDUCE y TO SMALLER region
+
+results = model.fit_generator(trainG, validation_data=testG, epochs=50, callbacks=callbacks)
