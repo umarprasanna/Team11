9486e767f8c3e198e9b3e19b25ef579231c6b091
umarprasanna <umarprasanna@gmail.com>
2018-11-26 19:55:03 -0500

    Update train_def.py

--- train_def.py
+++ train_def.py
@@ -65,9 +65,13 @@ def get_data(path,ids,train=True):
     print('Getting images ... ')
     for n, id_ in tqdm(enumerate(ids), total=len(ids)):
         # Load images
-        img = load_img(path + '/images/' + id_, grayscale=True)
-        x_img = img_to_array(img)
+        if train:
+           img = load_img(path + '/images/' + id_, grayscale=True)
+           
+        else:
+           img = load_img(path + 'test/images/' + id_, grayscale=True) #Change path for your system
 
+        x_img = img_to_array(img)
         # Load masks
         if train:
             mask = img_to_array(load_img(path + '/masks/' + id_, grayscale=True))
@@ -101,12 +105,28 @@ def add_random_gaussian_noise(image,mean,variance):
     noisy = image + gauss
     noisy_img_clipped = np.clip(noisy, 0, 255)
     return noisy_img_clipped
-    
 
-def image_generator(batch_size,train):
+  
+def horizontal_flip(image):
+    """Flip image left-right"""
+    image = cv2.flip(image, 1)
+    return image
+
+  
+def vertical_flip(image): 
+    """Flip image top-bottom"""
+    image = cv2.flip(image, 0)
+    return image
+  
+           
+def image_generator(batch_size,train,augment):
     """
-    Custom image generator that adds Gaussian noise and generates
+    Custom image generator that augments and generates
     batches of images and masks arrays
+    if train = True, generates images and masks else only images
+    if augment = noise, adds Gaussian noise
+    if augment = lr, flips horizontally
+    else, flips vertically
     
     """
     mean = 0.0
@@ -114,15 +134,20 @@ def image_generator(batch_size,train):
     border = 5
     
     path_train = "./"
+    path_test = "./"
     
     #list of files
     train_ids = next(os.walk(path_train+"images"))[2]
+    test_ids = next(os.walk(path_test+"test/images"))[2] # Change this for your system
+    if not train:
+       batch_size = 18000  #Fixed test size
     
     X = np.zeros((batch_size, 128, 128, 2), dtype=np.float32)
     if train:
         y = np.zeros((batch_size, 128, 128,1), dtype=np.float32)
-    
-    images,masks = get_data(path_train,train_ids,train=True) # Get arrays  
+        images,masks = get_data(path_train,train_ids,train=True) # Get arrays  
+    else:
+        images = get_data(path_train,test_ids,train=False) # Get arrays 
     
     batch_images = np.zeros((batch_size, 101,101,3))
     batch_masks = np.zeros((batch_size,101,101,1))
@@ -132,7 +157,13 @@ def image_generator(batch_size,train):
       raise ValueError("You cannot input less than 4000")
     
     for i in range(batch_size):
-        if i<4000:
+        if train:
+           n_samples = 4000 #train data size
+            
+        else:
+           n_samples = 18000 #test data size
+            
+        if i<n_samples:
            # choose random index in features
            batch_images[i] = images[i]
            if train:
@@ -159,10 +190,23 @@ def image_generator(batch_size,train):
             
         else:
            # choose random index in features
-           index= np.random.choice(4000,1)[0]
-           batch_images[i] = add_random_gaussian_noise(images[index],mean,variance)
-           if train:
-              batch_masks[i] = masks[index]
+           index= np.random.choice(n_samples,1)[0]
+           if augment == "noise":
+              batch_images[i] = add_random_gaussian_noise(images[index],mean,variance)
+           elif augment == "lr":
+              batch_images[i] = horizontal_flip(images[index])
+           else:
+              batch_images[i] = vertical_flip(images[index])
+              
+           if train: # For training we need masks as well
+              if augment == "noise":
+                 batch_masks[i] = masks[index]
+                  
+              elif augment == "lr":
+                 batch_masks[i] = resize(horizontal_flip(masks[index]), (101, 101, 1), mode='constant', preserve_range=True)
+                  
+              else:
+                 batch_masks[i] = resize(vertical_flip(masks[index]), (101, 101, 1), mode='constant', preserve_range=True)
 
            x_image = resize(batch_images[i], (128, 128, 1), mode='constant', preserve_range=True)
 
@@ -197,7 +241,7 @@ border = 5
 im_chan = 2 # Number of channels: first is original and second cumsum(axis=0)
 n_features = 1 # Number of extra features, like depth
 
-X, y = image_generator(8000,train=True)
+X, y = image_generator(8000,train=True,augment="noise")
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.15, random_state=42)
 
 save_str = 'model-tgs-salt-var-{0}--noise-doubled-'.format(v) + '-acc-{val_acc:.2f}-epoch-{epoch:02d}.h5'
@@ -210,5 +254,138 @@ callbacks = [
 model = UNET.U_Net()
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) # The mean_iou metrics seens to leak train and test values...
 
-results = model.fit({'img': X_train}, y_train, batch_size=16, epochs=100, callbacks=callbacks, validation_data=({'img': X_valid}, y_valid))
-   
+results = model.fit({'img': X_train}, y_train, batch_size=16, epochs=100, callbacks=callbacks, validation_data=({'img': X_valid}, y_valid))  
+
+# Hereon this code is predicting on test data and creating submission file
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    # src: https://www.kaggle.com/aglotero/another-iou-metric
+    labels = y_true_in
+    y_pred = y_pred_in
+    
+    true_objects = 2
+    pred_objects = 2
+
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins = true_objects)[0]
+    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+    
+    
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+  
+# Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+    
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+  
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.mean(metric)
+    
+def RLenc(img, order='F', format=True):
+    """
+    # Source https://www.kaggle.com/bguberfain/unet-with-depth
+    img is binary mask image, shape (r,c)
+    order is down-then-right, i.e.
+    format determines if the order needs to be preformatted (according to submission rules) or not
+
+    returns run length as an array or string (if format is True)
+    """
+    bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
+    runs = []  ## list of run lengths
+    r = 0  ## the current run length
+    pos = 1  ## count starts from 1 per WK
+    for c in bytes:
+        if (c == 0):
+            if r != 0:
+                runs.append((pos, r))
+                pos += r
+                r = 0
+            pos += 1
+        else:
+            r += 1
+
+    # if last run is unsaved (i.e. data ends with 1)
+    if r != 0:
+        runs.append((pos, r))
+        pos += r
+        r = 0
+
+    if format:
+        z = ''
+
+        for rr in runs:
+            z += '{} {} '.format(rr[0], rr[1])
+        return z[:-1]
+    else:
+        return runs
+    
+    
+#Get best threshold
+preds_val = model.predict({'img': X_valid}, verbose=1)
+thres = np.linspace(0.25, 0.75, 20)
+thres_iou = [iou_metric_batch(y_valid, np.int32(preds_val > t)) for t in tqdm(thres)]
+
+best_thres = thres[np.argmax(thres_iou)]
+best_thres, max(thres_iou)
+
+X_test = image_generator(18000,train=False,augment="lr")  # For test batch_size and augment parameters do not matter
+
+preds_test = model.predict(X_test,verbose=1)
+
+path_test = "./"
+    
+#list of test files
+test_ids = next(os.walk(path_test+"test/images"))[2] # Change this for your system
+
+# Reshape predicted mask
+preds_test_reshaped = []
+for i in range(len(preds_test)):
+    preds_test_reshaped.append(resize(np.squeeze(preds_test[i]), 
+                                       (101, 101), 
+                                       mode='constant', preserve_range=True))
+pred_dict = {id_[:-4]:RLenc(np.round(preds_test_reshaped[i] > 0.2)) for i,id_ in tqdm(enumerate(test_ids))}
+
+#Create submission file
+sub = pd.DataFrame.from_dict(pred_dict,orient='index')
+sub.index.names = ['id']
+sub.columns = ['rle_mask']
+sub.to_csv('submission.csv')
