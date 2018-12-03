691e8764f17c704d96f174e6f16cd718f2d9daf7
Adam Dawood <ayd5335@CSE-dynpool09.cse.psu.edu>
2018-12-03 18:49:53 -0500

    adds pred patch gen

--- vggGenerator.py
+++ vggGenerator.py
@@ -15,7 +15,7 @@ import pdb
 
 class vggGenerator(keras.utils.Sequence):
 
-    def __init__(self, list_IDs, data_dir, batch_size=32, dim=(48,48), patch_size= 48, n_channels=1, n_classes=2, shuffle=True):
+    def __init__(self, list_IDs, data_dir, pred_size=4, batch_size=32, dim=(48,48), patch_size= 48, n_channels=1, n_classes=2, shuffle=True):
         self.dim = dim
         self.patch_size = patch_size
         self.batch_size = batch_size
@@ -24,6 +24,7 @@ class vggGenerator(keras.utils.Sequence):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
+        self.pred_size = pred_size
         self.on_epoch_end()
     def __len__(self):
         return int(np.floor(len(self.list_IDs) / self.batch_size))
@@ -44,7 +45,7 @@ class vggGenerator(keras.utils.Sequence):
 
     def __data_generation__(self, list_IDs_temp):
         X = np.empty(self.dim + (3, self.batch_size))
-        y = np.empty((self.batch_size,))
+        y = np.empty((self.pred_size, self.pred_size) + (self.batch_size,))
 
         for i, ID in enumerate(list_IDs_temp):
             # Load X
@@ -55,9 +56,11 @@ class vggGenerator(keras.utils.Sequence):
             start_2 = int(np.random.rand() * (x_img.shape[1] - self.patch_size))
             end_1 = start_1 + self.patch_size
             end_2 = start_2 + self.patch_size
+            ps1 = int((self.patch_size - self.pred_size)/2)
+            ps2 = int(self.patch_size - ((self.patch_size - self.pred_size)/2))
             X[:, :, :, i] = x_img[start_1:end_1, start_2:end_2, :]
             mask = img_to_array(load_img(self.data_dir + '/masks/' + ID, grayscale=True))
             mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
-            y[i] = mask[start_1 + int(self.patch_size/2), start_2 + int(self.patch_size/2), 0]
-        return X.reshape((self.batch_size, *self.dim, 3)), y
+            y[:,:,i] = mask[start_1 + ps1:start_1 + ps2, start_2 + ps1:start_2 + ps2, 0]
+        return X.reshape((self.batch_size, *self.dim, 3)), y.reshape((self.batch_size, self.pred_size, self.pred_size))
 
